c2dc0db7d74a87d88e8a060c331192b5b63b3c77
Akhilesh <119729949+AkhileshMalthi@users.noreply.github.com>
2023-11-20 20:07:37 +0530

    Add Changes in BackEnd

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,17 +1,21 @@
 package src;
 
+import com.sun.jdi.event.ExceptionEvent;
+
 import java.sql.*;
 
 public class BackEnd {
     private static final String url = "jdbc:mysql://localhost:3306/inventory";
     private static final String user = "root";
     private static final String password = "AKakhilesh#29";
+
+    private static final String tableName = "products";
     private static Connection con;
 
-    static void getTableData(String tableName) throws SQLException {
+    public static void viewAllProducts() throws SQLException {
         String sql = "SELECT * FROM "+tableName+" limit 10";
-        PreparedStatement pst = con.prepareStatement(sql);
-        ResultSet resultSet = pst.executeQuery();
+        PreparedStatement stmt = con.prepareStatement(sql);
+        ResultSet resultSet = stmt.executeQuery();
 
 // ------------------------------------------------------------------------------------
 //        while (resultSet.next()) {
@@ -42,19 +46,82 @@ public class BackEnd {
         }
     }
 
+    public static void addProduct(String... values) throws SQLException {
+
+        StringBuilder query = new StringBuilder("INSERT INTO " + tableName + " VALUES(");
+        for (int i = 0; i < values.length; i++) {
+            if (i == values.length-1) {
+                query.append("\"").append(values[i]).append("\")");
+            }
+            else {
+                query.append("\"").append(values[i]).append("\",");
+            }
+        }
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(query.toString());
+            stmt.executeUpdate();
+        }
+        catch (SQLIntegrityConstraintViolationException e) {
+            System.out.println("The Id already exits");
+        }
+    }
+
+    public static void updateCost(String id, String newValue) throws SQLException {
+
+        String query = "UPDATE products SET Cost = "+newValue+" WHERE id = "+id;
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void updateQuantity(String id, String newValue) throws SQLException {
+
+        String query = "UPDATE products SET Quantity = "+newValue+" WHERE id = "+id;
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void deleteProduct(String id) throws SQLException {
+
+        String query = "DELETE FROM products WHERE id = "+id;
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
     public static void main(String[] args) throws SQLException {
         try {
             // Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url, user, password);
-
-            if (con != null) {
-                System.out.println("Connection Successful");
-                getTableData("products");
-            } else {
-                System.out.println("Connection Unsuccessful");
-            }
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
+
+        if (con != null) {
+
+            System.out.println("Connection Successful");
+
+            // Functions to Implement
+
+        } else {
+            System.out.println("Connection Unsuccessful");
+        }
+
     }
 }
